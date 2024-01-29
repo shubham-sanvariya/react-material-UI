fbbede78329cb8becfe059e0723760c9d774caac
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-29 19:28:48 +0530

    routing Navbar sidebar feed rightbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import Sidebar from './components/Sidebar';
+import Feed from './components/Feed';
+import Rightbar from './components/RightBar';
+import { Box, Stack } from '@mui/material';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <Box>
+      <Navbar />
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
   );
 }
 
