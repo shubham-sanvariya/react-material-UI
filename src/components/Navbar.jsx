9208d87feec4acc1525509295414c8f2b2e7dddf
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-29 20:10:27 +0530

    adding icons and search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import { Pets } from '@mui/icons-material';
-import { AppBar, Toolbar, Typography, styled } from '@mui/material'
+import { Mail, Notifications, Pets } from '@mui/icons-material';
+import { AppBar, Avatar, Badge, Box, InputBase, Toolbar, Typography, styled } from '@mui/material'
 import React from 'react'
 
 const StyledToolbar = styled(Toolbar)({
@@ -7,12 +7,52 @@ const StyledToolbar = styled(Toolbar)({
     justifyContent: "space-between"
 });
 
+const Search = styled("div")(({ theme }) => ({
+    backgroundColor: "white",
+    padding: "0 10px",
+    borderRadius: theme.shape.borderRadius,
+    width: '40%'
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+    display: "none", alignItems:"center", gap: "20px",
+    [theme.breakpoints.up("sm")]:{
+        display: "flex"
+    }
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+    display: "flex", alignItems:"center", gap: "10px",
+    [theme.breakpoints.up("sm")]: {
+        display: "none"
+    }
+}));
+
 const Navbar = () => {
   return (
     <AppBar position='sticky'>
         <StyledToolbar>
-            <Typography variant='h6' sx={{ display: { xs: "none", sm: "block" } }}>NavBar</Typography>
+            <Typography variant='h6'
+             sx={{ display: { xs: "none", sm: "block" } }}>
+                NavBar
+            </Typography>
             <Pets sx={{ display: { xs: "block", sm: "none" } }}/>
+            <Search><InputBase placeholder='search...'/></Search>
+            <Icons>
+                <Badge badgeContent={4} color='error'>
+                    <Mail />
+                </Badge>
+                <Badge badgeContent={4} color='error'>
+                    <Notifications />
+                </Badge>
+                  <Avatar sx={{width : 30, height : 30}} src='https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'/>
+            </Icons>
+            <UserBox>
+                <Avatar
+                sx={{ width: 30, height: 30 }}
+                src='https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' />
+                <Typography variant='span'>John</Typography>
+            </UserBox>
         </StyledToolbar>
     </AppBar>
   )
