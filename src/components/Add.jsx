4587328becd2c16db00ddee237ce75e7ecfbb42d
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-30 10:19:12 +0530

    adding Add icon

---
+++ src/components/Add.jsx
@@ -0,0 +1,17 @@
+import { Fab, Tooltip } from '@mui/material'
+import React from 'react'
+import {Add as AddIcon} from '@mui/icons-material'
+
+function Add() {
+  return (
+    <>
+      <Tooltip title="Add" sx={{position: "fixed",bottom:20,left:{xs:"calc(50% - 25px)", md: 30}}}>
+        <Fab color='primary' aria-label='add'>
+            <AddIcon />
+        </Fab>
+      </Tooltip>
+    </>
+  )
+}
+
+export default Add
